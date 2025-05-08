bed62951e5262c9bd7b150ea9aca98fb14e6bb9d
pknowles <pknowles@users.noreply.github.com>
2025-05-07 22:25:51 -0700

    add mappedfile linear resize tests

--- benchmark.cpp
+++ benchmark.cpp
@@ -264,6 +264,7 @@ TEST(WriteFile, SequentialBlocks) {
                          i);
                  CHECK(fsync(f) == 0);
                  CHECK(munmap(mapped, size) == 0);
+                 close(f);
                  sync();
              })
 #endif
@@ -449,5 +450,396 @@ TEST(WriteMemory, SequentialBlocks) {
             decodeless::memory_writer f(reservedAddressSpace, 4);
             for (int32_t i = 0; i < numBlocksToWrite; ++i)
                 std::ranges::fill(f.createArray<int32_t>(numIntsPerBlock), i);
+        });
+}
+
+TEST(MapFile, LinearResize) {
+    const TmpFile    mappedFile("tmp.dat");
+    constexpr size_t reservedAddressSpace = 1024 * 1024 * 1024;
+    constexpr size_t mapAddressSpace = 1024 * 1024;
+    constexpr size_t stepSize = 1009;
+    printf("Mapping %zu bytes in increments of %zu bytes\n", mapAddressSpace, stepSize);
+    nb::Bench()
+        .minEpochTime(std::chrono::milliseconds(50))
+        .maxEpochTime(std::chrono::seconds(3))
+        .minEpochIterations(3)
+        //.warmup(1)
+        .relative(true)
+#ifdef _WIN32
+        .run("MapViewOfFile (slow; remap whole file)",
+             [&] {
+                 size_t pageSize = []() {
+                     SYSTEM_INFO info;
+                     GetSystemInfo(&info);
+                     return info.dwPageSize;
+                 }();
+
+                 HANDLE hFile = CreateFileW(mappedFile.path.generic_wstring().c_str(),
+                                            GENERIC_READ | GENERIC_WRITE, 0, nullptr, CREATE_ALWAYS,
+                                            FILE_ATTRIBUTE_NORMAL, nullptr);
+                 CHECK(hFile != INVALID_HANDLE_VALUE);
+                 size_t mappedBytes = 0;
+                 size_t mappedPages = 0;
+                 HANDLE hMap = nullptr;
+                 LPVOID mapped = nullptr;
+                 for (; mappedBytes + stepSize <= mapAddressSpace;) {
+                     if (mappedBytes + stepSize > mappedPages * pageSize) {
+                         mappedPages++;
+                         if (mapped) {
+                             CHECK(UnmapViewOfFile(mapped));
+                             CloseHandle(hMap);
+                         }
+                         {
+                             LARGE_INTEGER liSize;
+                             liSize.QuadPart = mappedPages * pageSize;
+                             CHECK(SetFilePointerEx(hFile, liSize, nullptr, FILE_BEGIN));
+                             CHECK(SetEndOfFile(hFile));
+                         }
+                         hMap = CreateFileMappingW(hFile, nullptr, PAGE_READWRITE, 0, 0, nullptr);
+                         CHECK(hMap != nullptr);
+                         mapped = MapViewOfFile(hMap, FILE_MAP_WRITE, 0, 0, mappedPages * pageSize);
+                         CHECK(mapped != nullptr);
+                         ankerl::nanobench::doNotOptimizeAway(mapped);
+                     }
+                     mappedBytes += stepSize;
+                     ankerl::nanobench::doNotOptimizeAway(mappedBytes);
+                 }
+                 CHECK(UnmapViewOfFile(mapped));
+
+                 CloseHandle(hMap);
+                 CloseHandle(hFile);
+             })
+        .run("MapViewOfFile (cheat: whole file up-front)",
+             [&] {
+                 size_t pageSize = []() {
+                     SYSTEM_INFO info;
+                     GetSystemInfo(&info);
+                     return info.dwPageSize;
+                 }();
+
+                 HANDLE hFile = CreateFileW(mappedFile.path.generic_wstring().c_str(),
+                                            GENERIC_READ | GENERIC_WRITE, 0, nullptr, CREATE_ALWAYS,
+                                            FILE_ATTRIBUTE_NORMAL, nullptr);
+                 CHECK(hFile != INVALID_HANDLE_VALUE);
+
+                 LARGE_INTEGER liSize;
+                 liSize.QuadPart = mapAddressSpace;
+                 CHECK(SetFilePointerEx(hFile, liSize, nullptr, FILE_BEGIN));
+                 CHECK(SetEndOfFile(hFile));
+                 HANDLE hMap = CreateFileMappingW(hFile, nullptr, PAGE_READWRITE, 0, 0, nullptr);
+
+                 CHECK(hMap != nullptr);
+                 size_t mappedBytes = 0;
+                 size_t mappedPages = 0;
+                 LPVOID mapped = nullptr;
+                 for (; mappedBytes + stepSize <= mapAddressSpace;) {
+                     if (mappedBytes + stepSize > mappedPages * pageSize) {
+                         mappedPages++;
+                         if (mapped)
+                             CHECK(UnmapViewOfFile(mapped));
+                         mapped = MapViewOfFile(hMap, FILE_MAP_WRITE, 0, 0, mappedPages * pageSize);
+                         CHECK(mapped != nullptr);
+                         ankerl::nanobench::doNotOptimizeAway(mapped);
+                     }
+                     mappedBytes += stepSize;
+                     ankerl::nanobench::doNotOptimizeAway(mappedBytes);
+                 }
+                 CHECK(UnmapViewOfFile(mapped));
+
+                 CloseHandle(hMap);
+                 CloseHandle(hFile);
+             })
+#else
+        .run("mmap",
+             [&] {
+                 size_t pageSize = sysconf(_SC_PAGESIZE);
+                 void* reserved = mmap(nullptr, reservedAddressSpace, PROT_NONE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_NORESERVE, -1, 0);
+                 int    f = open(mappedFile.path.c_str(), O_RDWR | O_CREAT | O_TRUNC, (mode_t)0600);
+                 size_t mappedBytes = 0;
+                 size_t mappedPages = 0;
+                 void*  mappedEnd = reserved;
+                 for (; mappedBytes + stepSize <= mapAddressSpace;) {
+                     if (mappedBytes + stepSize > mappedPages * pageSize) {
+                         size_t allocPages =
+                             (mappedBytes + stepSize + pageSize - 1) / pageSize - mappedPages;
+                         CHECK(ftruncate(f, mappedPages * pageSize) == 0);
+                         auto mapped =
+                             mmap(mappedEnd,
+                                            allocPages * pageSize,
+                                            PROT_WRITE,
+                                                      MAP_SHARED, f, 0);
+                         CHECK(mapped != nullptr);
+                         mappedEnd = (std::byte*)mapped + allocPages * pageSize;
+                         mappedPages += allocPages;
+                         ankerl::nanobench::doNotOptimizeAway(mapped);
+                     }
+                     mappedBytes += stepSize;
+                     ankerl::nanobench::doNotOptimizeAway(mappedBytes);
+                 }
+                 CHECK(munmap(reserved, reservedAddressSpace) == 0);
+                 close(f);
+             })
+#endif
+        .run("resizable_file", [&] {
+            decodeless::resizable_file m(mappedFile.path, reservedAddressSpace);
+            for (; m.size() + stepSize <= mapAddressSpace;) {
+                m.resize(m.size() + stepSize);
+                ankerl::nanobench::doNotOptimizeAway(m.size());
+                ankerl::nanobench::doNotOptimizeAway(m.data());
+            }
+        });
+}
+
+TEST(MapFile, LinearResizePages) {
+    const TmpFile    mappedFile("tmp.dat");
+    constexpr size_t reservedAddressSpace = 1024 * 1024 * 1024;
+    constexpr size_t mapAddressSpace = 1024 * 1024;
+#ifdef _WIN32
+    size_t pageSize = []() {
+        SYSTEM_INFO info;
+        GetSystemInfo(&info);
+        return info.dwPageSize;
+    }();
+#else
+    size_t pageSize = sysconf(_SC_PAGESIZE);
+#endif
+    printf("Mapping %zu bytes in increments of %zu bytes\n", mapAddressSpace, pageSize);
+    nb::Bench()
+        .minEpochTime(std::chrono::milliseconds(50))
+        .maxEpochTime(std::chrono::seconds(3))
+        .minEpochIterations(3)
+        //.warmup(1)
+        .relative(true)
+#ifdef _WIN32
+        .run("MapViewOfFile (slow; remap whole file)",
+             [&] {
+                 size_t pageSize = []() {
+                     SYSTEM_INFO info;
+                     GetSystemInfo(&info);
+                     return info.dwPageSize;
+                 }();
+
+                 HANDLE hFile = CreateFileW(mappedFile.path.generic_wstring().c_str(),
+                                            GENERIC_READ | GENERIC_WRITE, 0, nullptr, CREATE_ALWAYS,
+                                            FILE_ATTRIBUTE_NORMAL, nullptr);
+                 CHECK(hFile != INVALID_HANDLE_VALUE);
+                 for (size_t pages = 1; pages * pageSize <= mapAddressSpace; ++pages) {
+                     LARGE_INTEGER liSize;
+                     liSize.QuadPart = pages * pageSize;
+                     CHECK(SetFilePointerEx(hFile, liSize, nullptr, FILE_BEGIN));
+                     CHECK(SetEndOfFile(hFile));
+                     HANDLE hMap =
+                         CreateFileMappingW(hFile, nullptr, PAGE_READWRITE, 0, 0, nullptr);
+                     CHECK(hMap != nullptr);
+                     auto mapped =
+                         (int32_t*)MapViewOfFile(hMap, FILE_MAP_WRITE, 0, 0, pages * pageSize);
+                     CHECK(mapped != nullptr);
+                     ankerl::nanobench::doNotOptimizeAway(mapped);
+                     CHECK(UnmapViewOfFile(mapped));
+                     CloseHandle(hMap);
+                 }
+                 CloseHandle(hFile);
+             })
+        .run("MapViewOfFile (cheat: whole file up-front)",
+             [&] {
+                 size_t pageSize = []() {
+                     SYSTEM_INFO info;
+                     GetSystemInfo(&info);
+                     return info.dwPageSize;
+                 }();
+
+                 HANDLE hFile = CreateFileW(mappedFile.path.generic_wstring().c_str(),
+                                            GENERIC_READ | GENERIC_WRITE, 0, nullptr, CREATE_ALWAYS,
+                                            FILE_ATTRIBUTE_NORMAL, nullptr);
+                 CHECK(hFile != INVALID_HANDLE_VALUE);
+
+                 LARGE_INTEGER liSize;
+                 liSize.QuadPart = mapAddressSpace;
+                 CHECK(SetFilePointerEx(hFile, liSize, nullptr, FILE_BEGIN));
+                 CHECK(SetEndOfFile(hFile));
+                 HANDLE hMap = CreateFileMappingW(hFile, nullptr, PAGE_READWRITE, 0, 0, nullptr);
+                 CHECK(hMap != nullptr);
+
+                 for (size_t pages = 1; pages * pageSize <= mapAddressSpace; ++pages) {
+                     auto mapped =
+                         (int32_t*)MapViewOfFile(hMap, FILE_MAP_WRITE, 0, 0, pages * pageSize);
+                     CHECK(mapped != nullptr);
+                     ankerl::nanobench::doNotOptimizeAway(mapped);
+                     CHECK(UnmapViewOfFile(mapped));
+                 }
+
+                 CloseHandle(hMap);
+                 CloseHandle(hFile);
+             })
+#else
+        .run("mmap",
+             [&] {
+                 size_t pageSize = sysconf(_SC_PAGESIZE);
+                 void* reserved = mmap(nullptr, reservedAddressSpace, PROT_NONE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_NORESERVE, -1, 0);
+                 int    f = open(mappedFile.path.c_str(), O_RDWR | O_CREAT | O_TRUNC, (mode_t)0600);
+                 size_t pages;
+                 for (pages = 1; pages * pageSize <= mapAddressSpace; ++pages) {
+                     CHECK(ftruncate(f, pages * pageSize) == 0);
+                     auto mapped = (int32_t*)mmap((std::byte*)reserved + (pages - 1) * pageSize, pageSize, PROT_WRITE,
+                                                  MAP_SHARED, f, 0);
+                     ankerl::nanobench::doNotOptimizeAway(mapped);
+                 }
+                 CHECK(fsync(f) == 0);
+                 CHECK(munmap(reserved, reservedAddressSpace) == 0);
+             })
+#endif
+        .run("resizable_file", [&] {
+            decodeless::resizable_file m(mappedFile.path, reservedAddressSpace);
+            for (; m.size() + pageSize <= mapAddressSpace;) {
+                m.resize(m.size() + pageSize);
+                ankerl::nanobench::doNotOptimizeAway(m.size());
+                ankerl::nanobench::doNotOptimizeAway(m.data());
+            }
+        });
+}
+
+TEST(MapMemory, LinearResize) {
+    constexpr size_t reservedAddressSpace = 1024 * 1024 * 1024;
+    constexpr size_t mapAddressSpace = 1024 * 1024;
+    constexpr size_t stepSize = 1009;
+    printf("Mapping %zu bytes in increments of %zu bytes\n", mapAddressSpace, stepSize);
+    nb::Bench()
+        .minEpochTime(std::chrono::milliseconds(1000))
+        .maxEpochTime(std::chrono::seconds(3))
+        .minEpochIterations(3)
+        //.warmup(1)
+        .relative(true)
+#ifdef _WIN32
+        .run("VirtualAlloc",
+             [&] {
+                 size_t pageSize = []() {
+                     SYSTEM_INFO info;
+                     GetSystemInfo(&info);
+                     return info.dwPageSize;
+                 }();
+                 uint32_t* reserved =
+                     (uint32_t*)VirtualAlloc(0, reservedAddressSpace, MEM_RESERVE, PAGE_NOACCESS);
+                 size_t mappedBytes = 0;
+                 size_t mappedPages = 0;
+                 for (; mappedBytes + stepSize <= mapAddressSpace;) {
+                     if (mappedBytes + stepSize > mappedPages * pageSize) {
+                         size_t allocPages =
+                             (mappedBytes + stepSize + pageSize - 1) / pageSize - mappedPages;
+                         std::ignore =
+                             VirtualAlloc(((std::byte*)reserved) + mappedPages * pageSize,
+                                          allocPages * pageSize, MEM_COMMIT, PAGE_READWRITE);
+                         mappedPages += allocPages;
+                     }
+                     mappedBytes += stepSize;
+                     // do nothing with the memory
+                     ankerl::nanobench::doNotOptimizeAway(mappedBytes);
+                     ankerl::nanobench::doNotOptimizeAway(reserved);
+                 }
+                 CHECK(VirtualFree(reserved, 0, MEM_RELEASE));
+             })
+#else
+        .run("mmap",
+             [&] {
+                 size_t pageSize = sysconf(_SC_PAGESIZE);
+                 uint32_t* reserved =
+                     (uint32_t*)mmap(nullptr, reservedAddressSpace, PROT_NONE,
+                                     MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+                 CHECK(reserved != MAP_FAILED);
+                 size_t mappedBytes = 0;
+                 size_t mappedPages = 0;
+                 for (; mappedBytes + stepSize <= mapAddressSpace;) {
+                     if (mappedBytes + stepSize > mappedPages * pageSize) {
+                         size_t allocPages =
+                             (mappedBytes + stepSize + pageSize - 1) / pageSize - mappedPages;
+                         CHECK(mprotect((std::byte*)reserved + mappedPages * pageSize,
+                                        allocPages * pageSize,
+                                        PROT_READ | PROT_WRITE) == 0);
+                         mappedPages += allocPages;
+                     }
+                     mappedBytes += stepSize;
+                     // do nothing with the memory
+                     ankerl::nanobench::doNotOptimizeAway(mappedBytes);
+                     ankerl::nanobench::doNotOptimizeAway(reserved);
+                 }
+                 CHECK(munmap(reserved, reservedAddressSpace) == 0);
+             })
+#endif
+        .run("resizable_memory", [&] {
+            decodeless::resizable_memory m(0, reservedAddressSpace);
+            for (; m.size() + stepSize <= mapAddressSpace;) {
+                m.resize(m.size() + stepSize);
+                ankerl::nanobench::doNotOptimizeAway(m.size());
+                ankerl::nanobench::doNotOptimizeAway(m.data());
+            }
+        });
+}
+
+TEST(MapMemory, LinearResizePages) {
+    constexpr size_t reservedAddressSpace = 1024 * 1024 * 1024;
+    constexpr size_t mapAddressSpace = 1024 * 1024;
+#ifdef _WIN32
+    size_t pageSize = []() {
+        SYSTEM_INFO info;
+        GetSystemInfo(&info);
+        return info.dwPageSize;
+    }();
+#else
+    size_t pageSize = sysconf(_SC_PAGESIZE);
+#endif
+    printf("Mapping %zu bytes in increments of %zu bytes\n", mapAddressSpace, pageSize);
+    nb::Bench()
+        .minEpochTime(std::chrono::milliseconds(1000))
+        .maxEpochTime(std::chrono::seconds(3))
+        .minEpochIterations(3)
+        //.warmup(1)
+        .relative(true)
+#ifdef _WIN32
+        .run("VirtualAlloc",
+             [&] {
+                 size_t pageSize = []() {
+                     SYSTEM_INFO info;
+                     GetSystemInfo(&info);
+                     return info.dwPageSize;
+                 }();
+                 uint32_t* reserved =
+                     (uint32_t*)VirtualAlloc(0, reservedAddressSpace, MEM_RESERVE, PAGE_NOACCESS);
+                 size_t mappedPages = 0;
+                 for (; mappedPages * pageSize <= mapAddressSpace;) {
+                     std::ignore = VirtualAlloc(((std::byte*)reserved) + mappedPages * pageSize,
+                                                pageSize, MEM_COMMIT, PAGE_READWRITE);
+                     mappedPages++;
+                     // do nothing with the memory
+                     ankerl::nanobench::doNotOptimizeAway(mappedPages);
+                     ankerl::nanobench::doNotOptimizeAway(reserved);
+                 }
+                 CHECK(VirtualFree(reserved, 0, MEM_RELEASE));
+             })
+#else
+        .run("mmap",
+             [&] {
+                 size_t pageSize = sysconf(_SC_PAGESIZE);
+                 uint32_t* reserved =
+                     (uint32_t*)mmap(nullptr, reservedAddressSpace, PROT_NONE,
+                                     MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+                 CHECK(reserved != MAP_FAILED);
+                 size_t mappedPages = 0;
+                 for (; mappedPages * pageSize <= mapAddressSpace;) {
+                     CHECK(mprotect((std::byte*)reserved + mappedPages * pageSize, pageSize,
+                                    PROT_READ | PROT_WRITE) == 0);
+                     mappedPages++;
+                     // do nothing with the memory
+                     ankerl::nanobench::doNotOptimizeAway(mappedPages);
+                     ankerl::nanobench::doNotOptimizeAway(reserved);
+                 }
+                 CHECK(munmap(reserved, reservedAddressSpace) == 0);
+             })
+#endif
+        .run("resizable_memory", [&] {
+            decodeless::resizable_memory m(0, reservedAddressSpace);
+            for (; m.size() + pageSize <= mapAddressSpace;) {
+                m.resize(m.size() + pageSize);
+                ankerl::nanobench::doNotOptimizeAway(m.size());
+                ankerl::nanobench::doNotOptimizeAway(m.data());
+            }
         });
 }
